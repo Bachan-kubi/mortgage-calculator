925a02f70ff74a0a90ee96a60029a03326ee9040
Bachan-kubi <felixghagra@gmail.com>
2023-05-14 13:11:59 +0600

    loadn added

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import Navbar from "./components/parts/Navbar";
 import Results from "./components/parts/Results";
 import Tenure from "./components/parts/Tenure";
 import SliderSelect from "./components/parts/SliderSelect";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState({
+    homeValue: 3000,
+    downPayment: 3000 * 0.2,
+    loanAmount: 3000 * 0.8,
+    loanTerm: 5,
+    interestRate: 5
+  });
+  
   return (
-    <div className="App">
+    <div>
       <Container maxWidth='lg'>
         <Navbar />
-        <Grid container spacing={5}>
-          <Grid item md={6}>
-            <SliderSelect />
+        <Grid container spacing={4}>
+          <Grid item md={6} xs={12}>
+            <SliderSelect data={data} setData={setData} />
             <Tenure />
           </Grid>
-          <Grid item md={6}>
+          <Grid item md={6} xs={12}>
             <Results />
           </Grid>
         </Grid>
--- src/components/parts/SliderSelect.js
+++ src/components/parts/SliderSelect.js
@@ -1,20 +1,68 @@
 import React from "react";
 import SliderComponents from "../common/SliderComponents";
 
-const SliderSelect = () => {
+const SliderSelect = ({ data, setData }) => {
+  const max_limit = 10000;
+  console.log(data);
   return (
     <div>
       <SliderComponents
-        amount={3500}
+        amount={data.homeValue}
         label="Home Value"
         unit="$"
         min={1000}
-        max={10000}
-        defaultValue={3000}
+        max={max_limit}
+        defaultValue={data.homeValue}
+        value={data.homeValue}
         marks
         step={100}
-        onChange={(e)=>console.log(e.target.value)}
-
+        onChange={(e, value) =>
+          setData({
+            ...data,
+            homeValue: value,
+            downPayment: value * 0.2,
+            loanAmount: value * 0.8
+          })
+        }
+      />
+      <SliderComponents
+        amount={data.downPayment}
+        label="Down Payment"
+        unit="$"
+        min={1000}
+        max={10000}
+        defaultValue={data.downPayment}
+        value={data.downPayment}
+        marks
+        step={2000}
+        onChange={(e, value) => setData({
+            downPayment: value
+        })}
+      />
+      <SliderComponents
+        amount={data.loanAmount}
+        label="Loan Amount"
+        unit="$"
+        min={1000}
+        max={data.homeValue}
+        defaultValue={data.loanAmount}
+        value={data.loanAmount}
+        marks
+        step={100}
+        onChange={(e, value) => setData({
+            loanAmount: value
+        })}
+      />
+      <SliderComponents
+        amount={4000}
+        label="Interest Rate"
+        unit="%"
+        min={2}
+        max={18}
+        defaultValue={5}
+        marks
+        step={0.5}
+        onChange={(e) => console.log(e.target.value)}
       />
     </div>
   );
